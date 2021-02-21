c148e6d099f2589f23d482c97e8549c3e41ad232
Krish47007 <krishnendu.gupta00@gmail.com>
2021-02-21 08:11:21 +0530

    Cascading

--- Hibernate_Tutorial/src/main/java/org/krish/dto/Vehicle.java
+++ Hibernate_Tutorial/src/main/java/org/krish/dto/Vehicle.java
@@ -1,24 +1,53 @@
 package org.krish.dto;
 
-import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
-
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@Builder
 @Entity
-@Table(name = "VEHICLE")
 public class Vehicle {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Vehicle_ID")
-    private Long id;
-    @Column(name = "Vehicle_Name")
-    private String name;
+    @GeneratedValue
+    private int vehicleId;
+    private String vehicleName;
 
+   /* @ManyToOne
+    @JoinColumn(name = "User_ID")
+    private UserDetails userDetails;
+
+    public UserDetails getUserDetails() {
+        return userDetails;
+    }
+    public void setUserDetails(UserDetails userDetails) {
+        this.userDetails = userDetails;
+    }*/
+
+/*   @ManyToMany(mappedBy = "vList")
+   private List<UserDetails> listOfUsers = new ArrayList<>();
+
+
+    public List<UserDetails> getListOfUsers() {
+        return listOfUsers;
+    }
+
+    public void setListOfUsers(List<UserDetails> listOfUsers) {
+        this.listOfUsers = listOfUsers;
+    }*/
+
+    public int getVehicleId() {
+        return vehicleId;
+    }
+
+    public void setVehicleId(int vehicleId) {
+        this.vehicleId = vehicleId;
+    }
+
+    public String getVehicleName() {
+        return vehicleName;
+    }
+
+    public void setVehicleName(String vehicleName) {
+        this.vehicleName = vehicleName;
+    }
 }
--- Hibernate_Tutorial/src/main/java/org/krish/hibernate/util/HibernateUtil.java
+++ Hibernate_Tutorial/src/main/java/org/krish/hibernate/util/HibernateUtil.java
@@ -3,21 +3,10 @@ package org.krish.hibernate.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-
 public class HibernateUtil {
 
-    private static volatile SessionFactory factory = null;
-
-    public static SessionFactory getSessionFactory()
-    {
-        if(factory == null)
-        {
-            synchronized (HibernateUtil.class)
-            {
-                if(factory == null)
-                    factory = new Configuration().configure().buildSessionFactory();
-            }
-        }
+    public static SessionFactory getSessionFactory() {
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
         return factory;
     }
 }
